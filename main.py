e73cac22ce6964648b2d7109e8ef02c1ccc1f76a
starry-skies <mmazin2028@kehillahstudent.org>
2025-03-17 09:24:47 -0700

    done

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+text_dict = {}
+for char in text:
+    key = char
+    if char in text_dict:
+        text_dict[char] += 1
+    else:
+        text_dict[char] = 1
+print(text_dict)
+    
